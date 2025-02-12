a57dd45893ad71b3f0730cb5dbdd3091d7f86f02
savanzscode <cristianmichael106206@gmail.com>
2025-02-12 10:50:24 +0700

    all fungsi product page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -285,6 +285,127 @@ public function brand_store(Request $request)
         $img->resize(540,689,function($constraint){
             $constraint->aspectRatio();
         })->save($destinationPathThumbnails.'/'.$imageName);
+    }public function edit_product($id)
+    {
+        $product = Product::find($id);
+        $categories = Category::Select('id','name')->orderBy('name')->get();
+        $brands = Brand::Select('id','name')->orderBy('name')->get();
+
+
+
+        return view('admin.product-edit',compact('product','categories','brands'));
+    }public function update_product(Request $request)
+    {
+        $request->validate([
+            'name'=>'required',
+            'slug'=>'required|unique:products,slug,'.$request->id,
+            'category_id'=>'required',
+            'brand_id'=>'required',
+            'short_description'=>'required',
+            'description'=>'required',
+            'regular_price'=>'required',
+            'sale_price'=>'required',
+            'SKU'=>'required',
+            'stock_status'=>'required',
+            'featured'=>'required',
+            'quantity'=>'required',
+            'image'=>'mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $product = Product::find($request->id);
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->short_description = $request->short_description;
+        $product->description = $request->description;
+        $product->regular_price = $request->regular_price;
+        $product->sale_price = $request->sale_price;
+        $product->SKU = $request->SKU;
+        $product->stock_status = $request->stock_status;
+        $product->featured = $request->featured;
+        $product->quantity = $request->quantity;
+        $current_timestamp = Carbon::now()->timestamp;
+
+        if($request->hasFile('image'))
+        {
+            if(File::exists(public_path('uploads/products') . '/' . $product->image))
+            {
+                File::delete(public_path('uploads/products') . '/' . $product->image);
+            }
+            if(File::exists(public_path('uploads/products/thumbnails') . '/' . $product->image))
+            {
+                File::delete(public_path('uploads/products/thumbnails') . '/' . $product->image);
+            }
+
+            $image = $request->file('image');
+            $imageName = $current_timestamp . '.' . $image->extension();
+            $this->GenerateProductThumbnailImage($image, $imageName);
+            $product->image = $imageName;
+
+        }
+
+        $gallery_arr = array();
+        $gallery_images = "";
+        $counter = 1;
+
+        if($request->hasFile('images'))
+        {
+            foreach(explode(',', $product->images) as $ofile)
+{
+    if(File::exists(public_path('uploads/products') . '/' . $ofile))
+    {
+        File::delete(public_path('uploads/products') . '/' . $ofile);
+    }
+    if(File::exists(public_path('uploads/products/thumbnails') . '/' . $ofile))
+    {
+        File::delete(public_path('uploads/products/thumbnails') . '/' . $ofile);
+    }
+}
+
+            $allowedfileExtion = ['jpg', 'png', 'jpeg'];
+            $files = $request->file('images');
+            foreach($files as $file)
+            {
+                $gextension = $file->getClientOriginalExtension();
+                $gcheck = in_array($gextension, $allowedfileExtion);
+                if($gcheck)
+                {
+                    $gfileName = $current_timestamp . "-" . $counter . "." . $gextension;
+                    $this->GenerateProductThumbailImage($file, $gfileName);
+                    array_push($gallery_arr, $gfileName);
+                    $counter = $counter + 1;
+                }
+            }
+
+            $gallery_images = implode(',', $gallery_arr);
+
+        }
+        $product->images = $gallery_images;
+
+        $product->save();
+        return redirect()->route('admin.products')->with('status','Record has been updated successfully !');
+    }public function delete_product($id)
+    {
+        $product = Product::find($id);
+        if(File::exists(public_path('uploads/products') . '/' . $product->image))
+        {
+            File::delete(public_path('uploads/products') . '/' . $product->image);
+        }
+        if(File::exists(public_path('uploads/products/thumbnails') . '/' . $product->image))
+        {
+            File::delete(public_path('uploads/products/thumbnails') . '/' . $product->image);
+        }foreach(explode(',', $product->images) as $ofile)
+        {
+            if(File::exists(public_path('uploads/products') . '/' . $ofile))
+            {
+                File::delete(public_path('uploads/products') . '/' . $ofile);
+            }
+            if(File::exists(public_path('uploads/products/thumbnails') . '/' . $ofile))
+            {
+                File::delete(public_path('uploads/products/thumbnails') . '/' . $ofile);
+            }
+        }
+        $product->delete();
+        return redirect()->route('admin.products')->with('status','Record has been deleted successfully !');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -36,5 +36,8 @@ Route::middleware(['auth', AuthAdmin::class])->group(function(){
     Route::get('/admin/products',[AdminController::class,'products'])->name('admin.products');
     Route::get('/admin/product/add',[AdminController::class,'add_product'])->name('admin.product.add');
     Route::post('/admin/product/store',[AdminController::class,'product_store'])->name('admin.product.store');
+    Route::get('/admin/product/{id}/edit',[AdminController::class,'edit_product'])->name('admin.product.edit');
+    Route::put('/admin/product/update',[AdminController::class,'update_product'])->name('admin.product.update');
+    Route::delete('/admin/product/{id}/delete',[AdminController::class,'delete_product'])->name('admin.product.delete');
 
 });
