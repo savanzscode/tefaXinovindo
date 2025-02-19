3df20c44652e4e18b64320ecc8d9c973e0b476b5
savanzscode <cristianmichael106206@gmail.com>
2025-02-19 16:37:52 +0700

    menyelesaikan sitem sorting category dan price list

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -14,6 +14,9 @@ class ShopController extends Controller
         $size = $request->query('size', 12);
         $order = $request->query('order', -1);
         $f_brands = $request->query('brands');
+        $f_categories = $request->query('categories');
+        $min_price = $request->query('min')?$request->query('min'):1;
+        $max_price = $request->query('max')?$request->query('max'):10000;
         $orderOptions = [
             1 => ['created_at', 'DESC'],
             2 => ['created_at', 'ASC'],
@@ -23,15 +26,21 @@ class ShopController extends Controller
 
 
         $brands = Brand::orderBy('name','ASC')->get();
+        $categories = Category::orderBy('name','ASC')->get();
 
         [$o_column, $o_order] = $orderOptions[$order] ?? ['id', 'DESC'];
 
         $products = Product::when(!empty($f_brands), function($query) use ($f_brands) {
             $query->whereIn('brand_id', explode(',', $f_brands));
-    })
+    })->when(!empty($f_categories), function($query) use ($f_categories) {
+        $query->whereIn('category_id', explode(',', $f_categories));
+})->where(function($query) use($min_price,$max_price){
+    $query->whereBetween('regular_price',[$min_price,$max_price])
+    ->orWhereBetween('sale_price',[$min_price,$max_price]);
+})
         ->orderBy($o_column, $o_order)->paginate($size);
 
-        return view('shop', compact('products', 'size', 'order','brands','f_brands'));
+        return view('shop', compact('products', 'size', 'order','brands','f_brands','categories','f_categories','max_price','min_price'));
     }
 
 
